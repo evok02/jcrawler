4231f7fc8dc493461b86b9ffc0d2951cd6ee7551
evok02 <hlibter1002@gmail.com>
2026-02-14 14:09:32 +0100

    added filter.go

--- internal/filter/filter.go
+++ internal/filter/filter.go
@@ -1,15 +1,20 @@
 package filter
 
 import (
+	"fmt"
 	"crypto/sha256"
 	"github.com/evok02/jcrawler/internal/parser"
 	"hash"
 	"regexp"
 	"time"
+	"github.com/evok02/jcrawler/internal/db"
+	"errors"
 )
 
 // filter should check for link entry in the db
 // filter should check if the provided entry is a link
+
+var ERROR_MALICIOUS_URL_FORMAT = errors.New("malicious url format")
 
 type Filter struct {
 	timeout time.Duration
@@ -23,11 +28,43 @@ func NewFilter(t time.Duration) *Filter {
 	}
 }
 
+func (f *Filter) IsValid(link parser.Link, s *db.Storage) (bool, error) {
+	url := link.URL
+	if !f.isUrl(url) {
+		return false, ERROR_MALICIOUS_URL_FORMAT
+	}
+
+	return f.checkTimeout(s, url) 
+}
+
 func (f *Filter) isUrl(link string) bool {
-	validUrl := regexp.MustCompile(`b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b`)
+	validUrl := regexp.MustCompile(`\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b`)
 	return validUrl.MatchString(link)
 }
 
-func (f *Filter) checkIfAlreadyExist(l parser.Link) (bool, error) {
-	return false, nil
+func (f *Filter) hashLink(link string) ([]byte, error) {
+	_, err := f.hash.Write([]byte(link))
+	if err != nil {
+		return nil, fmt.Errorf("hashLink: %s", err.Error())
+	}
+
+	return f.hash.Sum(nil), nil
+}
+
+func (f *Filter) checkTimeout(s *db.Storage, link string) (bool, error) {
+	hashed, err := f.hashLink(link)
+	if err != nil {
+		return false, fmt.Errorf("checkIfAlreadyExist: %s", err.Error())
+	}
+
+	p, err := s.GetPageByID(string(hashed))
+	if err != nil {
+		return false, fmt.Errorf("checkIfAlreadyExist: %s", err.Error())
+	}
+
+	if (time.Since(p.UpdatedAt) < f.timeout) {
+		return false, nil
+	}
+	
+	return true, nil
 }
